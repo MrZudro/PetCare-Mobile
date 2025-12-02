95fcd99e874ac7b69d0e91af7bcf13a4bea4fbc5
EivorKinKest <jmqo2015@gmail.com>
2025-12-02 17:10:11 -0500

    Chore: Cnfiguracion de temas, assets, y configuraciones generales

---
+++ lib/core/color_theme.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF00BFFF);
+  static const Color secondary = Color(0xFFFF69B4);
+  static const Color tertiary = Color(0xFF8A2BE2);
+  static const Color accentOne = Color(0xFFB2E1FF);
+  static const Color accentTwo = Color(0xFFFFB3D9);
+  static const Color accentThree = Color(0xFFD6C9FF);
+
+  //Color de algunas utilidades
+  static const Color primaryText = Color(0xFF000000);
+  static const Color secondaryText = Color(0xFFFFFFFF);
+
+  //Background colors
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color backgroundSecondary = Color(0xFFF0F8FF);
+
+  //Colores semanticos
+  static const Color success = Color(0xFF90EE90);
+  static const Color error = Color(0xFFFF6347);
+  static const Color warning = Color(0xFFFFD700);
+}
---
+++ lib/core/text_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle bodyText = TextStyle(
+    fontSize: 18, 
+    color: Colors.white,
+    fontFamily:'Poppins',
+    );
+
+    static const TextStyle titleText = TextStyle(
+      fontSize: 24,
+      color: Colors.white,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.bold,
+    );
+}
