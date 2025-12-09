bd25f8dc52afc59a8742fd3844d8cf787112de33
EivorKinKest <jmqo2015@gmail.com>
2025-12-09 16:00:07 -0500

    Chore: soporte para emulacion en dispositivos fisicos

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:petcare/core/api_constants.dart';
 import 'package:petcare/pages/auth.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ApiConstants.init();
   runApp(const MainApp());
 }
 
